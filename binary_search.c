90861e88e67d8f4fb47db3244f457ddcef3a338b
krishna16600 <krishna.malhotra691692@gmail.com>
2019-07-12 20:58:07 +0530

    updated binary search

--- binary_search.c
+++ binary_search.c
@@ -16,15 +16,32 @@ int findIndex(int arr[],int s,int e,int elem)
 }
 int main()
 {
+	
 	int size=0;
 	printf("enter the size of the array");
 	scanf("%d",&size);
+	//binary search requires sorted array as input to find the element
 	printf("enter the elements of the array");
 	int arr[size];
 	for(int i=0;i<size;i++)
 	{
 		scanf("%d",&arr[i]);
 	}
+	//arranging in ascending  sorted order
+		for(int i=0;i<size-1;i++)
+	      	{
+			for(int j=0;j<size-1-i;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+
+
 	int el=0;
 	printf("enter the element you want to search");
 	scanf("%d",&el);
